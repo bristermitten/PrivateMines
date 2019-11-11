6b5d961c61f68147d9d5e2b519211ade698a1320
UntouchedOdin0 <UntouchedOdin0@users.noreply.github.com>
2019-11-11 17:46:00 +0000

    Update PrivateMinesCommand.java

--- src/main/java/me/bristermitten/privatemines/commands/PrivateMinesCommand.java
+++ src/main/java/me/bristermitten/privatemines/commands/PrivateMinesCommand.java
@@ -1,9 +1,26 @@
 package me.bristermitten.privatemines.commands;
 
-import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
-@CommandAlias("privatemines|privatemine|pm")
-public class PrivateMinesCommand extends BaseCommand {
+public class PrivateMinesCommand implements CommandExecutor {
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String s, String[] strings) {
+
+        Player p = (Player) sender;
+
+        String permission = "privatemines.owner.";
+
+        if (cmd.getName().equalsIgnoreCase("privatemines")) {
+            if (!p.hasPermission(permission)) {
+                p.sendMessage("Opening public mines gui.");
+            } else {
+                p.sendMessage("Opening private mines owner gui.");
+            }
+        }
+        return false;
+    }
 }
