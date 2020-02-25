d3d50ec164ba5a6e78c328cab3a60186c77164f3
UntouchedOdin0 <UntouchedOdin0@users.noreply.github.com>
2020-02-25 21:18:21 +0000

    This is just getting started. I need to work on this.
    Signed-off-by: UntouchedOdin0 <UntouchedOdin0@users.noreply.github.com>

--- src/main/java/me/bristermitten/privatemines/view/ChangeThemeMenu.java
+++ src/main/java/me/bristermitten/privatemines/view/ChangeThemeMenu.java
@@ -1,5 +1,50 @@
 package me.bristermitten.privatemines.view;
 
+import me.bristermitten.privatemines.PrivateMines;
+import me.bristermitten.privatemines.Util;
+import me.bristermitten.privatemines.config.PMConfig;
+import me.bristermitten.privatemines.config.menu.MenuConfig;
+import me.bristermitten.privatemines.config.menu.MenuSpec;
+import me.bristermitten.privatemines.data.PrivateMine;
+import me.bristermitten.privatemines.service.MineStorage;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Handles the menu to change a Private Mine theme
+ */
 public class ChangeThemeMenu {
 
+    private static MenuSpec original;
+
+    public ChangeThemeMenu(Player p, PrivateMines plugin, PMConfig config, MenuConfig menuConfig, MineStorage storage, File f) {
+        if (original == null) {
+            original = new MenuSpec();
+            original.loadFrom(menuConfig.configurationForName("Blocks"));
+        }
+
+        MenuSpec spec = new MenuSpec();
+        spec.copyFrom(original);
+        spec.register(plugin);
+        PrivateMine mine = storage.getOrCreate(p, f);
+        Material[] blocks = config.getBlockOptions().toArray(new Material[]{});
+        p.openInventory(spec.genMenu((block, i) -> {
+                    i.setType(block);
+                    ItemMeta itemMeta = i.getItemMeta();
+                    String displayName = itemMeta.getDisplayName();
+                    String name = displayName.replace("%block%", Util.prettify(block.name()));
+                    itemMeta.setDisplayName(name);
+                    List<String> lore = itemMeta.getLore().stream().map(s -> s.replace("%block%",
+                            Util.prettify(block.name()))).collect(Collectors.toList());
+                    itemMeta.setLore(lore);
+                    i.setItemMeta(itemMeta);
+                    return i;
+                },
+                block -> e -> mine.setBlock(block), blocks));
+    }
 }
