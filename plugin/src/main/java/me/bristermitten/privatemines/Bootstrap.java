e49ab97e12c42217ca1dbe1e38d800e811104321
Alexander Wood <alexljwood24@hotmail.co.uk>
2020-12-27 13:53:57 +0000

    Bring the jar size down a bit, worldguardwrapper adds quite a lot :(

---
+++ plugin/src/main/java/me/bristermitten/privatemines/Bootstrap.java
@@ -0,0 +1,14 @@
+package me.bristermitten.privatemines;
+
+import me.bristermitten.privatemines.worldedit.LegacyWEHook;
+import me.bristermitten.privatemines.worldedit.ModernWEHook;
+
+public class Bootstrap {
+    public static void main(String[] args) {
+        PrivateMines.MINES_FILE_NAME.hashCode();
+        new LegacyWEHook();
+        new ModernWEHook();
+
+        System.out.println("This class exists to stop shadowJar from minimizing everything :)");
+    }
+}
