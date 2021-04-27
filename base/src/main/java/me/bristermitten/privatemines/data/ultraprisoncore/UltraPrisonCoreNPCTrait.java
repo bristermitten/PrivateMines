553d89598539997212408ab1e0f7b041066ba929
Alexander Wood <alexljwood24@hotmail.co.uk>
2021-04-27 22:25:31 +0100

    More cleanup

--- base/src/main/java/me/bristermitten/privatemines/data/ultraprisoncore/UltraPrisonCoreNPCTrait.java
+++ base/src/main/java/me/bristermitten/privatemines/data/ultraprisoncore/UltraPrisonCoreNPCTrait.java
@@ -11,30 +11,25 @@ import me.drawethree.ultraprisoncore.UltraPrisonCore;
 import me.drawethree.ultraprisoncore.api.events.UltraPrisonAutoSellEvent;
 import me.drawethree.ultraprisoncore.api.events.UltraPrisonSellAllEvent;
 import me.drawethree.ultraprisoncore.autosell.UltraPrisonAutoSell;
-import net.citizensnpcs.api.event.NPCLeftClickEvent;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
-import net.citizensnpcs.api.persistence.DelegatePersistence;
 import net.citizensnpcs.api.persistence.Persist;
-import net.citizensnpcs.api.persistence.Persister;
 import net.citizensnpcs.api.trait.Trait;
-import net.citizensnpcs.api.util.DataKey;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.inventory.ItemStack;
 
 import java.text.DecimalFormat;
-import java.util.List;
 import java.util.UUID;
 
 public class UltraPrisonCoreNPCTrait extends Trait implements Listener {
 
+    private static final DecimalFormat DECIMAL_FORMAT = new DecimalFormat("#.##");
     private final MineStorage storage = PrivateMines.getPlugin().getStorage();
     private final BukkitCommandManager manager = PrivateMines.getPlugin().getManager();
-    private final DecimalFormat df = new DecimalFormat("#.##");
     PrivateMine privateMine;
 
+    @SuppressWarnings("unused")
     @Persist("owner")
     private UUID owner;
     private Double tax;
@@ -42,21 +37,6 @@ public class UltraPrisonCoreNPCTrait extends Trait implements Listener {
 
     public UltraPrisonCoreNPCTrait() {
         super("UltraPrisonCoreNPC");
-    }
-
-    public UltraPrisonCore getCore() {
-        return UltraPrisonCore.getInstance();
-    }
-
-    public void setOwner(UUID owner) {
-        this.owner = owner;
-    }
-
-    @EventHandler
-    public void onLeftClick(NPCLeftClickEvent e) {
-        if (!e.getNPC().equals(npc)) {
-            return;
-        }
     }
 
     /*
@@ -72,6 +52,7 @@ public class UltraPrisonCoreNPCTrait extends Trait implements Listener {
         if (e.getClicker() == null) {
             return;
         }
+
         e.getClicker().performCommand("sellall");
     }
 
@@ -82,29 +63,15 @@ public class UltraPrisonCoreNPCTrait extends Trait implements Listener {
         BukkitCommandIssuer ownerIssuer = manager.getCommandIssuer(Bukkit.getPlayer(owner));
 
         manager.sendMessage(issuer, MessageType.INFO, LangKeys.INFO_TAX_TAKEN,
-                "{amount}", String.valueOf(df.format(tax)),
+                "{amount}", String.valueOf(DECIMAL_FORMAT.format(tax)),
                 "{tax}", String.valueOf(privateMine.getTax()));
 
         if (Bukkit.getOnlinePlayers().contains(Bukkit.getPlayer(owner))) {
 
             manager.sendMessage(ownerIssuer, MessageType.INFO, LangKeys.INFO_TAX_RECIEVED,
-                    "{amount}", String.valueOf(df.format(tax)),
+                    "{amount}", String.valueOf(DECIMAL_FORMAT.format(tax)),
                     "{tax}", String.valueOf(privateMine.getTax()));
         }
-    }
-
-    private boolean eventIsNotApplicable(List<ItemStack> itemsSold, Player player) {
-        if (itemsSold.isEmpty()) {
-            return true;
-        }
-        if (player.getUniqueId().equals(owner)) {
-            return true;
-        }
-        PrivateMine privateMine = storage.get(owner);
-        if (privateMine == null) {
-            return true;
-        }
-        return !privateMine.contains(player);
     }
 
     private boolean eventIsNotApplicable(Player player) {
@@ -119,9 +86,6 @@ public class UltraPrisonCoreNPCTrait extends Trait implements Listener {
     }
 
 
-    /*
-        UltraPrisonCore Sell System Listeners..
-     */
 
     @EventHandler
     public void onUPCAutoSellEvent(UltraPrisonAutoSellEvent e) {
@@ -129,24 +93,18 @@ public class UltraPrisonCoreNPCTrait extends Trait implements Listener {
         privateMine = storage.get(owner);
 
         UltraPrisonCore core = UltraPrisonCore.getInstance();
-        UltraPrisonAutoSell autoSell = new UltraPrisonAutoSell(core);
-        if (autoSell.isEnabled()) {
-            double earnings = autoSell.getCurrentEarnings(player);
-            double taxEarnings = (earnings / 100.0) * privateMine.getTaxPercentage();
-            e.setMoneyToDeposit(earnings);
-            process(privateMine, taxEarnings, player);
-        } else {
+        UltraPrisonAutoSell autoSell = core.getAutoSell();
+        if (!autoSell.isEnabled()) {
             return;
         }
+        double earnings = autoSell.getCurrentEarnings(player);
+        double taxEarnings = earnings / 100.0 * privateMine.getTaxPercentage();
+        e.setMoneyToDeposit(earnings);
+        process(privateMine, taxEarnings, player);
     }
 
     @EventHandler
     public void onUPCSellAllEvent(UltraPrisonSellAllEvent e) {
-
-        /*
-            Need to add when more things are added to this API.
-         */
-
         Player player = e.getPlayer();
         privateMine = storage.get(owner);
 
@@ -156,29 +114,24 @@ public class UltraPrisonCoreNPCTrait extends Trait implements Listener {
             return;
         }
 
-        try {
-            if (privateMine.getTaxPercentage() > 0) {
-                try {
-                    tax = privateMine.getTaxPercentage();
-                } catch (NullPointerException exc) {
-                    defaultTax = PrivateMines.getPlugin().getConfig().getDouble("Tax-Percentage");
-                }
-            } else if (privateMine.getTaxPercentage() == 0) {
-                return;
+        if (privateMine.getTaxPercentage() > 0) {
+            try {
+                tax = privateMine.getTaxPercentage();
+            } catch (NullPointerException exc) {
+                defaultTax = PrivateMines.getPlugin().getConfig().getDouble("Tax-Percentage");
             }
-            double d = e.getSellPrice();
-
-            if (tax == 0) {
-                tax = defaultTax;
-            }
-            double takenTax = d / 100.0 * tax;
-
-            double afterTax = d - takenTax;
-            e.setSellPrice(afterTax);
-            process(privateMine, takenTax, player);
-        } catch (NullPointerException e1) {
-            // do nothing, its a false exception.
+        } else if (privateMine.getTaxPercentage() == 0) {
             return;
         }
+        double d = e.getSellPrice();
+
+        if (tax == 0) {
+            tax = defaultTax;
+        }
+        double takenTax = d / 100.0 * tax;
+
+        double afterTax = d - takenTax;
+        e.setSellPrice(afterTax);
+        process(privateMine, takenTax, player);
     }
 }
