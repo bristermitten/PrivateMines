d5349be10730ca7319189f2e5e57f823c6e8fb4e
UntouchedOdin0 <unconfigured@null.spigotmc.org>
2021-04-28 15:01:19 +0100

    Let's hit this class with the NotNull effect! :)

--- base/src/main/java/me/bristermitten/privatemines/world/EmptyWorldGenerator.java
+++ base/src/main/java/me/bristermitten/privatemines/world/EmptyWorldGenerator.java
@@ -2,12 +2,19 @@ package me.bristermitten.privatemines.world;
 
 import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Random;
 
 public class EmptyWorldGenerator extends ChunkGenerator {
+
     @Override
-    public ChunkData generateChunkData(World world, Random random, int cx, int cz, BiomeGrid biome) {
+    @NotNull
+    public ChunkData generateChunkData(@NotNull World world,
+                                       @NotNull Random random,
+                                       int cx,
+                                       int cz,
+                                       @NotNull BiomeGrid biome) {
         return createChunkData(world);
     }
 }
